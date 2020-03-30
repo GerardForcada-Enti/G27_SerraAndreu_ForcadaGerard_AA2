59caf9f9389f7bf1a9e278558f0186f73565a0d1
Gerard <geri271098@gmail.com>
2020-03-30 10:24:23 +0200

    border collisions

--- G27_SerraAndreu_ForcadaGerard_AA2/Map.cpp
+++ G27_SerraAndreu_ForcadaGerard_AA2/Map.cpp
@@ -19,11 +19,11 @@ void Map::InitializeMap()
 			for (int j = 0; j < this->height; j++)
 			{
 				m_map[i][j] = aux2[j];
-				if(aux2[j]=='1')//Pasar pos Player1
+				if(aux2[j] == '1')//Pasar pos Player1
 				{
 					m_players[0] = Player(i, j);
 				}
-				if(aux2[j]=='2')//Pasar pos Player2
+				if(aux2[j] == '2')//Pasar pos Player2
 				{
 					m_players[1] = Player(i, j);
 				}
@@ -39,70 +39,79 @@ void Map::UpdateMap(InputData keyboard)
 {
 	if (keyboard.keys[(int)InputKey::UP1]) 
 	{ 
-		if (this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y - 1] != 'X' || this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y - 1] != '*')
+		if (this->m_players[0].GetPos().x <= 1) {}
+		else if (this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y - 1] != 'X' || this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y - 1] != '*')
 		{
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = ' ';
 			m_players[0].MovePlayer(MOVEMENT::UP);
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = '1';
 		}
 	}
-	else if (keyboard.keys[(int)InputKey::UP2]) 
-	{
-		if (this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y - 1] != 'X' || this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y - 1] != '*')
-		{
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
-			m_players[1].MovePlayer(MOVEMENT::UP);
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
-		}	
-	}
 	else if (keyboard.keys[(int)InputKey::DOWN1])
 	{
-		if (this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y + 1] != 'X' || this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y + 1] != '*')
+		if (this->m_players[0].GetPos().x >= this->width - 2) {}
+		else if (this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y + 1] != 'X' || this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y + 1] != '*')
 		{
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = ' ';
 			m_players[0].MovePlayer(MOVEMENT::DOWN);
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = '1';
-		}	
-	}
-	else if (keyboard.keys[(int)InputKey::DOWN2])
-	{
-		if (this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y + 1] != 'X' || this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y + 1] != '*')
-		{
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
-			m_players[1].MovePlayer(MOVEMENT::DOWN);
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
 		}
 	}
 	else if (keyboard.keys[(int)InputKey::RIGHT1])
 	{
-		if (this->m_map[m_players[0].GetPos().x + 1][m_players[0].GetPos().y] != 'X' || this->m_map[m_players[0].GetPos().x + 1][m_players[0].GetPos().y] != '*')
+		if (this->m_players[0].GetPos().y >= this->height - 2) {}
+		else if (this->m_map[m_players[0].GetPos().x + 1][m_players[0].GetPos().y] != 'X' || this->m_map[m_players[0].GetPos().x + 1][m_players[0].GetPos().y] != '*')
 		{
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = ' ';
 			m_players[0].MovePlayer(MOVEMENT::RIGHT);
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = '1';
 		}
 	}
-	else if (keyboard.keys[(int)InputKey::RIGHT2])
-	{
-		if (this->m_map[m_players[1].GetPos().x + 1][m_players[1].GetPos().y] != 'X' || this->m_map[m_players[1].GetPos().x + 1][m_players[1].GetPos().y] != '*')
-		{
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
-			m_players[1].MovePlayer(MOVEMENT::RIGHT);
-			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
-		}
-	}
 	else if (keyboard.keys[(int)InputKey::LEFT1])
 	{
-		if (this->m_map[m_players[0].GetPos().x - 1][m_players[0].GetPos().y] != 'X' || this->m_map[m_players[0].GetPos().x - 1][m_players[0].GetPos().y] != '*')
+		if (this->m_players[0].GetPos().y <= 1) {}
+		else if (this->m_map[m_players[0].GetPos().x - 1][m_players[0].GetPos().y] != 'X' || this->m_map[m_players[0].GetPos().x - 1][m_players[0].GetPos().y] != '*')
 		{
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = ' ';
 			m_players[0].MovePlayer(MOVEMENT::LEFT);
 			this->m_map[m_players[0].GetPos().x][m_players[0].GetPos().y] = '1';
 		}
 	}
+
+	if (keyboard.keys[(int)InputKey::UP2]) 
+	{
+		if (this->m_players[1].GetPos().x <= 1) {}
+		else if (this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y - 1] != 'X' || this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y - 1] != '*')
+		{
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
+			m_players[1].MovePlayer(MOVEMENT::UP);
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
+		}	
+	}
+	else if (keyboard.keys[(int)InputKey::DOWN2])
+	{
+		if (this->m_players[1].GetPos().x >= this->width - 2) {}
+		else if (this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y + 1] != 'X' || this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y + 1] != '*')
+		{
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
+			m_players[1].MovePlayer(MOVEMENT::DOWN);
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
+		}
+	}
+	else if (keyboard.keys[(int)InputKey::RIGHT2])
+	{
+		if (this->m_players[1].GetPos().y >= this->height - 2) {}
+		else if (this->m_map[m_players[1].GetPos().x + 1][m_players[1].GetPos().y] != 'X' || this->m_map[m_players[1].GetPos().x + 1][m_players[1].GetPos().y] != '*')
+		{
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
+			m_players[1].MovePlayer(MOVEMENT::RIGHT);
+			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = '2';
+		}
+	}
 	else if (keyboard.keys[(int)InputKey::LEFT2])
 	{
-		if (this->m_map[m_players[1].GetPos().x - 1][m_players[1].GetPos().y] != 'X' || this->m_map[m_players[1].GetPos().x - 1][m_players[1].GetPos().y] != '*')
+		if (this->m_players[1].GetPos().y <= 1) {}
+		else if (this->m_map[m_players[1].GetPos().x - 1][m_players[1].GetPos().y] != 'X' || this->m_map[m_players[1].GetPos().x - 1][m_players[1].GetPos().y] != '*')
 		{
 			this->m_map[m_players[1].GetPos().x][m_players[1].GetPos().y] = ' ';
 			m_players[1].MovePlayer(MOVEMENT::LEFT);
@@ -118,7 +127,6 @@ void Map::PrintMap()
 	{
 		for (int j = 0; j < this->height; j++)
 		{
-
 			if(m_map[i][j]=='1')SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 170);//player 1
 			else if (m_map[i][j] == '2')SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 221);//player 2
 			else if (m_map[i][j] == 'X')SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 136);//muros x
